166d2ce16974d4c4671fffc0d9cac5ceff84e1bd
Leonardo Machado <lmachado25@gmail.com>
2022-03-10 14:02:29 -0300

    modificar version azurerm

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      #version = "=2.98.0"
-      version = "=2.46.1"
+      version = "=2.98.0"
     }
   }
 }
