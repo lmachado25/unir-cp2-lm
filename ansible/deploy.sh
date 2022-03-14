0da9ef855d84a395e09354d2cf494fef97ec4f18
Leonardo Machado <lmachado25@gmail.com>
2022-03-14 17:37:31 +0000

    Adecuar repo para entrega final

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,3 @@
-ansible-playbook -i initial -l initial initial.yaml -v
 ansible-playbook -i hosts -l nfs node_nfs.yaml -v
 ansible-playbook -i hosts -l master node_master.yaml -v
 ansible-playbook -i hosts -l workers node_workers.yaml -v
